1244fd39d18a795eb53f37676a90d96f0c8ab92c
Unknown <daniel.j.klug@gmail.com>
2020-03-01 18:44:51 -0800

    Refactoring to change behavior

--- playbackTool.py
+++ playbackTool.py
@@ -1,198 +1,193 @@
-# ========================================================================== #
-# PlaybackTool2
-# ========================================================================== #
+# ============================================================================ #
+# PlaybackTool
+# ============================================================================ #
 '''
 README.md goes here
 '''
-# -------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
 # Imports
-
-import maya.OpenMayaUI as mui  # Handling Maya QT widgets
-import maya.api.OpenMaya as api  # Handling callbacks
 from maya import cmds, mel  # As always...
-import time  # Toggle key timing
+
+# My modules
+from KlugTools.Utils import timeline_overlay
 
 # Vendor import
-from Qt import QtGui, QtCore, QtWidgets, __binding__ 
-if "PyQt" == __binding__:
-    import sip
-elif "PySide" == __binding__:
-    import shiboken as shiboken  # Do Pyside
-elif "PySide2" == __binding__:
-    import shiboken2 as shiboken # You're on Maya 2018+
+from Qt import QtGui, QtCore, QtWidgets, QtCompat
 
-
-# -------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
 # Globals
 
 PBT = None
 playbackFPS = 24  # Personal pref
 
 
-# -------------------------------------------------------------------------- #
+# ============================================================================ #
+# Event filter --------------------------------------------------------------- #
+
+class UI_Event_Filter(QtCore.QObject):
+    def __init__(self, parent):
+        super(UI_Event_Filter, self).__init__()
+        self._parent = parent
+        self.clicks = [0, 0, 0]
+    
+    def check_clicks(self):
+        check  = QtWidgets.QApplication.instance().mouseButtons()
+        left   = bool(QtCore.Qt.LeftButton   & check)
+        middle = bool(QtCore.Qt.MiddleButton & check)
+        right  = bool(QtCore.Qt.RightButton  & check)
+        self.clicks = [int(left), int(middle), int(right)]
+
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.MouseButtonPress:
+            if self._parent.mouse_activation_state == False: # Prevent it from firing twice
+                self._parent.mouse_activation_state = True
+            
+                if self._parent.is_active == True:
+                    if self._parent.key_activation_state == True:
+                        self.check_clicks()
+                        if self.clicks == [1, 0, 0]: # Left click only
+                            self._parent.scrub_frame_indicator()
+                            if self._parent.did_scrub == False: # If not yet scrubbing
+                                self._parent.start_scrub()
+            
+        if event.type() == QtCore.QEvent.MouseButtonRelease:
+            if self._parent.mouse_activation_state == True: # Prevent it from firing twice
+                self._parent.mouse_activation_state = False
+
+                if self._parent.is_active == True:
+                    if self._parent.key_activation_state == False:
+                        print "Cleanup step"
+                        self.check_clicks()
+                        if self.clicks == [0, 0, 0]: # Mouse empty
+                            self._parent.cleanup()
+                        
+            if event.type() == QtCore.QEvent.ApplicationStateChange:
+                self._parent.cleanup()
+                print 'Maya focus lost! Aborting!'
+
+        return QtCore.QObject.eventFilter(self, obj, event)
+
+
+# ---------------------------------------------------------------------------- #
 # Classes
 
 class PlaybackTool(QtWidgets.QWidget):
     """Docstring for PlaybackTool
-    
-    Don't run this directly.
+
+    Don't run this directly. Use the interface methods.
 
     """
+
     def __init__(self):
         super(PlaybackTool, self).__init__()
-        self.setObjectName('TimelineGhost')
+        self.setObjectName('PlaybackTool')
+        
+        self.is_active = False
+        self.key_activation_state = False
+        self.is_playing    = cmds.play(q=True, state=True)
+        self.did_scrub     = False
+        self.mouse_activation_state = False
+        self.panel_under_pointer = None
+        
+        self.filters_installed = False
+        self.ui_event_filter = UI_Event_Filter(self)
+        self.install_event_filters()
 
-        self.initUI()
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.previous_frame  = cmds.currentTime(q=True)
+        self.indicator_frame = self.previous_frame
+        self.timeline_indicator = timeline_overlay.Timeline_Overlay()
+        self.timeline_indicator.show()
+        
+        self.timeline_indicator.set_color([255, 0, 120])
+        self.timeline_indicator.set_alpha(100)
+        self.timeline_indicator.set_frames(self.previous_frame)
+        
 
-        self.start         = None
-        self.end           = None
-        self.total         = None
-        self.brush_width   = None
-        self.step          = None
+    def install_event_filters(self):
+        if not self.filters_installed:
+            QtWidgets.QApplication.instance().installEventFilter(self.ui_event_filter)
+        self.filters_installed = True
 
-        self.isPlaying     = cmds.play(q=True, state=True)
-        self.isScrubbing   = False
-        self.play_hold     = False
-
-        time_init = cmds.currentTime(query=True)
-        self.firstFrame    = time_init
-        self.lastFrame     = time_init
-        self.ghost_frame   = time_init
-        self.current_frame = time_init
-
-        self.callback_IDs = {}
-        self._install_callbacks()
-
-    def initUI(self):
-        ptr = mui.MQtUtil.findControl(self._get_maya_timeline())
-        timelineWidget = shiboken.wrapInstance(long(ptr), QtWidgets.QWidget)
-
-        internalWidget = timelineWidget.children()[0]
-        try:
-            lyt = internalWidget.children()[0]
-        except:
-            timelineWidget.layout().setContentsMargins(0, 0, 0, 0)
-            lyt = QtWidgets.QVBoxLayout()
-            lyt.setContentsMargins(0, 0, 0, 0)
-            lyt.setSpacing(0)
-            internalWidget.setLayout(lyt)
-        lyt.addWidget(self)
+    def uninstall_event_filters(self):
+        if self.filters_installed:
+            QtWidgets.QApplication.instance().removeEventFilter(self.ui_event_filter)
+        self.filters_installed = False
 
     def closeEvent(self, e):
-        for callback_ID in self.callback_IDs.values():
-            api.MMessage.removeCallback(callback_ID)
-
-    def _install_callbacks(self):
-        self.callback_IDs['timechanged'] = \
-            api.MEventMessage.addEventCallback(
-                'timeChanged', self.on_time_changed)
-        self.callback_IDs['playingBack'] = \
-            api.MConditionMessage.addConditionCallback(
-                'playingBack', self.playingBack)
-
-    def playingBack(self, isPlaying, _):
-        self.isPlaying = isPlaying
-        if isPlaying:
-            self.isScrubbing = not cmds.play(q=True, state=True)
-            self.ghost_frame = cmds.currentTime(q=True)
-        else:
-            self.isScrubbing = False
-            self.current_frame = cmds.currentTime(q=True)    
-        self.update()
-
-    def on_time_changed(self, *args):
-        if self.isPlaying:
-            return
-        if self.isScrubbing:
-            return
-        frame = cmds.currentTime(q=True)
-        self.ghost_frame = self.current_frame
-        self.current_frame = frame
-        self.update()
-
-
-    def paintEvent(self, e):
-        painter = QtGui.QPainter()
-        painter.begin(self)
-        self.draw(painter)
-        painter.end()
-
-    def draw(self, painter):
-        self.start = cmds.playbackOptions(query=True, min=True)
-        self.end   = cmds.playbackOptions(query=True, max=True)
-
-        self.total = self.width()
-        self.step = (self.total * 0.99) / (self.end-self.start+1)
-
-        pen = QtGui.QPen()
-        pen.setWidth(self.step)
-        pen.setColor(QtGui.QColor(0, 150, 255, 100))  # I shouldn't hardcode the color...
-
-        frame_pos = (self.ghost_frame - self.start) * self.step + (self.step / 2) + ((self.total * 0.01) * 0.5)
-        line = QtCore.QLineF(
-            QtCore.QPointF(frame_pos, 0),
-            QtCore.QPointF(frame_pos, 200))
-
-        painter.setPen(pen)
-        painter.drawLine(line)
-
-    def _get_timeline_range(self):
-        time_range = cmds.timeControl(self._get_maya_timeline(), q=True, ra=True )
-        return range(int(time_range[0]), int(time_range[1]))
-
-    def _get_maya_timeline(self):
-        return mel.eval("$tmpVar=$gPlayBackSlider")
+        self.timeline_indicator.close()
+        self.timeline_indicator.deleteLater()
+        self.uninstall_event_filters()
 
 
     # -------------------------------------------------------------------------- #
     # Functions
 
+    def start_scrub(self):
+        print "Start scrub"
+        self.did_scrub = True # Bounce out of playback after scrubbing.
+        # Get into the timescrub
+        self.panel_under_pointer = cmds.getPanel(underPointer=True)
+        mel.eval('storeLastAction("restoreLastContext " + `currentCtx`)')
+        cmds.setToolTo('TimeDragger')
+
+    def stop_scrub(self):
+        print "Stop scrub"
+        self.is_active = False
+
+        
+        if self.panel_under_pointer:
+            if cmds.getPanel(typeOf=self.panel_under_pointer) == 'modelPanel':
+                cmds.evalDeferred("mel.eval('invokeLastAction')")
+                return
+        mel.eval('invokeLastAction') # For everything else not modelPanel
+
     def press(self):
-        # Start the timer
-        self.TIME_START = time.time()
+        print "Press"
+        self.is_active = True
+        self.did_scrub = False
+        self.key_activation_state = True
+        self.is_playing = cmds.play(q=True, state=True)
 
-        self.isPlaying = cmds.play(q=True, state=True)
-
-        if self.isPlaying:
-            cmds.play(state = False)
-            self.play_hold = True
-        else:
-            self.firstFrame = cmds.currentTime(q=True)
-
-        # The only annoying thing is the flashing in&out of the scrub context
-        # whether or not we use it.
-        # TODO: Investigate a workaround that doesn't use callbacks.
-        # I suppose we could be listening for left clicks while the spacebar
-        # is held down...
-        mel.eval('storeLastAction( "restoreLastContext " + `currentCtx` )')
-        cmds.setToolTo( 'TimeDragger' )
 
     def release(self):
+        print "Release"
 
-        mel.eval('invokeLastAction')
+        self.key_activation_state = False
 
-        if (time.time() - self.TIME_START) < 0.15:  # Quick button press
-            if not self.isPlaying and not self.play_hold:
-                cmds.playbackOptions(edit=True, playbackSpeed = playbackFPS / 24.0)
-                cmds.play(state = True)
-        
-        self.play_hold = False
+        if self.did_scrub == False:
+            self.is_active = False
+            if self.is_playing:
+                cmds.play(state=False) # Stop
+                self.is_playing = False
+            else:
+                cmds.playbackOptions(edit=True, playbackSpeed=playbackFPS / 24.0)
+                cmds.play(state=True) # Start
+                self.is_playing = True
+                self.scrub_frame_indicator()
+        else: # Did scrub
+            if self.mouse_activation_state == False:
+                self.cleanup()
+
+    def cleanup(self):
+        self.stop_scrub()
 
     def toggle(self):
-        play_check = self.isPlaying
-        if play_check:  # Or else Maya will ignore the playhead
-            cmds.play(state=False)
-
-        temp_frame = cmds.currentTime(q=True)
-        cmds.currentTime(self.ghost_frame)
-
-        if not play_check:
-            self.ghost_frame = temp_frame
-            self.update()
+        if self.is_playing:
+            cmds.play(state=False) # Stop
+            cmds.currentTime(self.indicator_frame)
+            cmds.play(state=True) # Start
+            
         else:
-            self.update()
-            cmds.play(state=True)
+            self.previous_frame = cmds.currentTime(q=True)
+            cmds.currentTime(self.indicator_frame)
+            self.timeline_indicator.set_frames(self.previous_frame)
+            self.indicator_frame = self.previous_frame
+
+    def scrub_frame_indicator(self):
+        self.indicator_frame = cmds.currentTime(q=True)
+        self.timeline_indicator.set_frames(self.indicator_frame)
+
+
 
 # ========================================================================== #
 # The interface:
@@ -214,7 +209,14 @@ def release():
 def toggle():
     PBT.toggle()
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+def kill():
+    global PBT
+    PBT.close()
+    PBT.deleteLater()
+    PBT = None
+    
 # ========================================================================== #
 # Manual Override
 
@@ -227,9 +229,10 @@ if __name__ == "__main__":
 
     PBT = PlaybackTool()
 
-
 # ========================================================================== #
 # Notes
+
 '''
-Needs polish...
+Needs feedback for behavior
+
 '''
